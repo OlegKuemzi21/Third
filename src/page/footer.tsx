b7554ddb1cf8f9be65903f5f1c0dceeb21029884
OlegKuemzi21 <gamjoly@gmail.com>
2023-04-02 16:12:05 +0300

    cha

--- src/page/footer.tsx
+++ src/page/footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
     <div className="bg-borderB py-12  text-center mt-28 ">
-      <h3 className="font-bold text-lg italic ">My website</h3>
+      <h3 className="font-bold text-lg italic ">My website.com</h3>
     </div>
   );
 };
